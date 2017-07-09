6027be021b74b92aad2fdb74b348fb185e8bd7f9
ashvithashetty <ashvithashetty26@gmail.com>
2017-07-10 00:06:43 +0530

    updated setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'enhance_contrast',
+  packages = ['enhance_contrast'], # this must be the same as the name above
+  version = '0.3',
+  description = 'Enhance image contrast using standard deviation stretch',
+  author = 'Ashvitha Shetty',
+  author_email = 'ashvithashetty26@gmail.com',
+  url = 'https://github.com/ashvithashetty/contrast_enhancement', # use the URL to the github repo
+  download_url = 'https://github.com/ashvithashetty/enhance_contrast/archive/0.3.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
