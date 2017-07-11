1198a9841d6b3f8383d5a14fa2d98bf939e035eb
ashvitha <ashvithashetty26@gmail.com>
2017-07-11 20:20:03 +0530

    updated setup file

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
   name = 'contrast_enhancement',
   packages = ['ce'], 
-  version = '0.4',
+  version = '0.5',
   description = 'Enhance image contrast using standard deviation stretch',
   author = 'Ashvitha Shetty',
   author_email = 'ashvithashetty26@gmail.com',
   url = 'https://github.com/ashvithashetty/contrast_enhancement',
-  download_url = 'https://github.com/ashvithashetty/contrast_enhancement/archive/0.4.tar.gz', 
+  download_url = 'https://github.com/ashvithashetty/contrast_enhancement/archive/0.5.tar.gz', 
   classifiers = [],
 )
